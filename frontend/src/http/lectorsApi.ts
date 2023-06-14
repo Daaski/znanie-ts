e63c37827fd75925438d4982d89074726e494c4f
DarknessInsideAll <Redkin.Artem.2017@yandex.ru>
2023-06-14 21:16:34 +0300

    cors

--- frontend/src/http/lectorsApi.ts
+++ frontend/src/http/lectorsApi.ts
@@ -1,13 +1,19 @@
 import * as T from './types';
 import { $host } from 'http/index';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export const getFilterLectors: T.GetFilterLectors = async (q) => {
     q = q?.toLowerCase();
-    return await $host.get(`api/users/lectors/?surname=${q}`).json();
+    try {
+        return await $host.get(`api/users/lectors/?surname=${q}`).json();
+    } catch (e: any) {
+        console.log(e.message);
+    }
 };
 
 export const getFilterLectorsFullName: T.GetFilterFullNameLectors = async (
-    fullname
+    fullname,
 ) => {
     fullname = fullname?.toLowerCase();
     return await $host.get(`api/users/lectors/?fullname=${fullname}`).json();
