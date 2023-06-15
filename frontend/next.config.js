5166dc6e741e61b1f438d41a40287b33ac99f4aa
DarknessInsideAll <Redkin.Artem.2017@yandex.ru>
2023-06-15 23:17:09 +0300

    fix lector

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://51.250.80.71:8080/api/:path*'
+                destination: 'http://localhost:8080/api/:path*'
             }
         ]
     },
