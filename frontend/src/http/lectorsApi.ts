ad64e1e33ad8966bca493bd25bd8f9cca7aca400
DarknessInsideAll <Redkin.Artem.2017@yandex.ru>
2023-06-15 23:52:14 +0300

    fix lector

--- frontend/src/http/lectorsApi.ts
+++ frontend/src/http/lectorsApi.ts
@@ -3,16 +3,16 @@ import { $host } from 'http/index';
 
 export const getFilterLectors: T.GetFilterLectors = async (q) => {
     q = q?.toLowerCase();
-    return await $host.get(`api/users/lectors/?surname=${q}`).json();
+    return await $host.get(`/api/users/lectors/?surname=${q}`).json();
 };
 
 export const getFilterLectorsFullName: T.GetFilterFullNameLectors = async (
     fullname
 ) => {
     fullname = fullname?.toLowerCase();
-    return await $host.get(`api/users/lectors/?fullname=${fullname}`).json();
+    return await $host.get(`/api/users/lectors/?fullname=${fullname}`).json();
 };
 
 export const getLector: T.GetLector = async (id) => {
-    return await $host.get(`api/users/lectors/${id}/`).json()
+    return await $host.get(`/api/users/lectors/${id}/`).json()
 }
