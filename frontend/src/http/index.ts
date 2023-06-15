f72dcddfb9868130d95c7fb23b40623f144eff84
DarknessInsideAll <Redkin.Artem.2017@yandex.ru>
2023-06-16 01:05:19 +0300

    fix lector

--- frontend/src/http/index.ts
+++ frontend/src/http/index.ts
@@ -3,11 +3,25 @@ import * as process from 'process';
 
 export const $host = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
+    proxy: {
+        host: 'http://51.250.80.71',
+        port: 8080
+    }
 });
 
 
 export const $auth = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
+    proxy: {
+        host: 'http://51.250.80.71',
+        port: 8080
+    }
 });
 
 $auth.interceptors.request.use(res => {
