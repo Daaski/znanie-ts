e430fac0e7a153ec1732b9558a731ffed46c8df0
DarknessInsideAll <Redkin.Artem.2017@yandex.ru>
2023-06-16 01:52:46 +0300

    fix lector

--- backend/znanie_russia/settings.py
+++ backend/znanie_russia/settings.py
@@ -12,32 +12,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('POSTGRES_DB')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://51.250.80.71:3000',]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost', 'http://127.0.0.1:3000', 'http://51.250.80.71:3000',]
-
-CORS_ALLOW_HEADERS = (
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-)
-
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
